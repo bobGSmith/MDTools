a3af4289245241f2abeb1808d44080ba0d551d50
Bob G Smith <bobbyatopk@gmail.com>
2023-06-01 13:31:58 +0100

    installable

---
+++ MDTools/__init__.py
@@ -0,0 +1,22 @@
+'''MDTools 
+
+A collection of functions/ scripts for working with markdown documents 
+
+scripts 
+=======
+scripts can be called with the following command: 
+
+> python3 -m MDTools.SCRIPT_NAME ARGS 
+
+The following scripts are included: 
+* MDTools.abbreviate : finds first instance of a phrase that you want to 
+  abbreviate, adds the abbreviation behind it in brackets, and then replaces all future instances. 
+* MDTools.CSV2MD: converts csv format to markdown format tables 
+* MDTools.MultiTOC: takes a directory of markdown files and subdirectories
+  containing more markdown files, then creates a hyperlinked table of contents in a 
+  readme.me at the root of the dir
+* MDTools.TOC: Adds a table of contents to a single markdown document at the top (with hyperlinks) 
+'''
+
+print(__doc__)
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup 
+
+setup (
+    name = 'MDTools',
+    version = '1.0.0',
+    description = 'Scripts and functions for working with markdown files',
+    url = 'https://github.com/bobGSmith/MDTools',
+    author = 'Bob G Smith',
+    author_email = 'bobbyatopk@gmail.com',
+    license = 'MIT',
+    packages = ['MDTools'],
+    install_requires = [],
+    classifiers = ['Development Status :: 2 - Pre-Alpha','Intended Audience :: End Users/Desktop','License :: MIT','Operating System :: OS Independent','Programming Language :: Python :: 3.8']
+)
+
+
+__version__ = '1.0.0'
+__author__ = 'Bob G Smith'
